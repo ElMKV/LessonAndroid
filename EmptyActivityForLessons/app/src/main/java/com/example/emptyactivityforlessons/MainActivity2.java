f87c5679e8d706efaf9cb53d52dfdce2d14cc364
ELMKV <ilya.makoveev021199@gmail.com>
2020-09-11 15:18:15 +0500

    Изучил жтзненнный цикл на примере двух активностей

--- EmptyActivityForLessons/app/src/main/java/com/example/emptyactivityforlessons/MainActivity2.java
+++ EmptyActivityForLessons/app/src/main/java/com/example/emptyactivityforlessons/MainActivity2.java
@@ -1,38 +1,30 @@
 package com.example.emptyactivityforlessons;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.MenuInflater;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
-
+public class MainActivity2 extends AppCompatActivity {
 
 
     String TAG = "LifeCycle";
 
-    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        button = (Button) findViewById(R.id.button);
-        Log.d(TAG, "MainActivity onCreate");
+        setContentView(R.layout.activity_main2);
+        Log.d(TAG, "MainActivity2 onCreate");
     }
 
 
     @Override
     protected void onRestart() {
         super.onRestart();
-        Log.d(TAG, "MainActivity onRestart");
+        Log.d(TAG, "MainActivity2 onRestart");
 
 
     }
@@ -40,41 +32,35 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     protected void onStart() {
         super.onStart();
-        Log.d(TAG, "MainActivity onStart");
+        Log.d(TAG, "MainActivity2 onStart");
     }
 
 
     @Override
     protected void onResume() {
         super.onResume();
-        Log.d(TAG, "MainActivity onResume");
+        Log.d(TAG, "MainActivity2 onResume");
 
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-        Log.d(TAG, "MainActivity onPause");
+        Log.d(TAG, "MainActivity2 onPause");
     }
 
     @Override
     protected void onStop() {
         super.onStop();
-        Log.d(TAG, "MainActivity onStop");
+        Log.d(TAG, "MainActivity2 onStop");
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        Log.d(TAG, "MainActivity onDestroy ");
+        Log.d(TAG, "MainActivity2 onDestroy ");
 
     }
 
-    @Override
-    public void onClick(View view) {
 
-        Intent intent = new Intent(this, MainActivity2.class);
-        startActivity(intent);
-
-    }
 }
