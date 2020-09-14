ac7eb2acbc1b88f2b9678c74c4f635158523a915
ELMKV <ilya.makoveev021199@gmail.com>
2020-09-15 03:19:01 +0500

    Научился спользовать Intent ACTION

--- EmptyActivityForLessons/app/src/main/java/com/example/emptyactivityforlessons/MainActivity.java
+++ EmptyActivityForLessons/app/src/main/java/com/example/emptyactivityforlessons/MainActivity.java
@@ -3,16 +3,57 @@ package com.example.emptyactivityforlessons;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.MenuInflater;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button buttonCall;
+    Button buttonWeb;
+    Button buttonMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        buttonCall = findViewById(R.id.buttonCall);
+        buttonMap = findViewById(R.id.buttonMap);
+        buttonWeb = findViewById(R.id.buttonWeb);
+
+        buttonWeb.setOnClickListener(this);
+        buttonMap.setOnClickListener(this);
+        buttonCall.setOnClickListener(this);
+
+
     }
-    
+
+    @Override
+    public void onClick(View view) {
+
+        Intent intent;
+
+        switch (view.getId()) {
+            case R.id.buttonWeb:
+                intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.mmbyte.com/article/8117.html"));
+                startActivity(intent);
+                break;
+            case R.id.buttonMap:
+                intent = new Intent();
+                intent.setAction(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse("geo:-0.45609946, -90.2611607513"));
+                startActivity(intent);
+                break;
+            case R.id.buttonCall:
+                intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse("tel:12345"));
+                startActivity(intent);
+                break;
+        }
+
+    }
 }
